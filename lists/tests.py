14fb2c3e979a1114f3af698c6c41d4c8589d6454
Santana_MASG <elmickeyguzman@hotmail.com>
2019-12-08 03:36:49 -0600

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page 
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 class HomePageTest(TestCase):
@@ -22,10 +23,36 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 1)
+
+    def home_page(request):
+        if request.method == 'POST':
+            Item.objects.create(text=request.POST['item_text'])
+            return redirect('/')
+
+        return render(request, 'home.html')
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
